c1ec163d0d000c959fdf1ae2863782003e9c57d7
Maksims Salajevs <maximdtt@gmail.com>
2024-05-12 18:48:04 +0100

    no message

--- MathTrainer/TrainViewController.swift
+++ MathTrainer/TrainViewController.swift
@@ -9,10 +9,32 @@ import Foundation
 import UIKit
 
 final class TrainViewController: UIViewController {
+    
     // MARK: - Properties
     var type: MathTypes = .add {
         didSet {
             print(type)
+        }
+    }
+    @IBOutlet var buttonsCollection: [UIButton]!
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureButtons()
+    }
+    
+    // MARK: - Methods
+    
+    private func configureButtons() {
+        
+        //add shadow
+        buttonsCollection.forEach { button in
+            button.layer.shadowColor = UIColor.darkGray.cgColor
+            button.layer.shadowOffset = CGSize.init(width: 0, height: 2)
+            button.layer.shadowOpacity = 0.4
+            button.layer.shadowRadius = 3
         }
     }
 }
