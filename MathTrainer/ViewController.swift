0e4d609d14bc72668758a0d275636e69de38c7f7
Maksims Salajevs <maximdtt@gmail.com>
2024-05-12 14:06:00 +0100

    Initial Commit

---
+++ MathTrainer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathTrainer
+//
+//  Created by Maksims Šalajevs on 12/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
